e94763aed66826a4bfc4123c710d61828ef3f608
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-12 01:53:33 +0200

    Change - rename text center

--- peterchrjoergensen/header.php
+++ peterchrjoergensen/header.php
@@ -36,7 +36,7 @@
         <?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<header class="container header text-center">
+		<header class="container header u-text-center">
 			<div class="row">
 				<section class="u-full-width">
 					<!-- Title -->
