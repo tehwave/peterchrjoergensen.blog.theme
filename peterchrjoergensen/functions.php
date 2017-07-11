0f3e0b4717765f7f41e3e796bbf10f246acb787d
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-11 17:27:25 +0200

    Add - jquery

--- peterchrjoergensen/functions.php
+++ peterchrjoergensen/functions.php
@@ -3,7 +3,9 @@
 // Stylesheets
 function pcj_add_stylesheets() {
 	wp_register_script('pcj.css', get_template_directory_uri() . '/assets/css/pcj.min.css', array(),'1.0');
-	wp_enqueue_script('pcj.css');
+
+	wp_register_script('jquery.js', 'https://code.jquery.com/jquery-1.12.4.min.js', array(),'1.0');
+	wp_enqueue_script('jquery.js');
 }
 
 add_action('wp_enqueue_scripts', 'pcj_add_stylesheets');
