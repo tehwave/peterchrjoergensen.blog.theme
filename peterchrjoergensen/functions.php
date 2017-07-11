bcfd7c6f6618bdbad8fb3b9151dc69c508eecb6f
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-11 17:32:13 +0200

    Add - jquery local file

--- peterchrjoergensen/functions.php
+++ peterchrjoergensen/functions.php
@@ -13,7 +13,7 @@ function pcj_add_scripts() {
 	wp_register_script('parallax.js', get_template_directory_uri() . '/assets/js/parallax.min.js', array(),'1.0');
 	wp_enqueue_script('parallax.js');
 
-	wp_register_script('jquery.js', 'https://code.jquery.com/jquery-1.12.4.min.js', array(),'1.0');
+	wp_register_script('jquery.js', get_template_directory_uri() . 'jquery-1.12.4.min', array(),'1.0');
 	wp_enqueue_script('jquery.js');
 }
 
