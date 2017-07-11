48c68427977e11e09f655cb035d468ea998e650c
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-11 19:57:10 +0200

    Add - introduction

--- peterchrjoergensen/index.php
+++ peterchrjoergensen/index.php
@@ -4,11 +4,24 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-<main class="u-full-width">
-	<h1>Test</h1>
-</main>
+		<!-- Main -->
+		<main class="u-full-width">
+			<!-- Introduction -->
+			<section class="parallax intro" data-parallax="scroll" data-speed="0.6" data-image-src="<?php echo esc_url( get_template_directory_uri() ) ?>/assets/img/index.jpg" data-natural-width="1920" data-natural-height="1080" data-z-index="0">
+				<div class="container">
+					<h2>Welcome,</h2>
+					<p>This is my blog.</p>
+					<p><?php echo bloginfo('description'); ?></p>
+				</div>
+			</section>
+
+			<!-- Latest Posts -->
+			<div class="container projects text-center">
+			</div>
+		</main>
 
 <?php get_footer();
