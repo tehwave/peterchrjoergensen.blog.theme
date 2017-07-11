eee0513d5a50753483a583c67eb1ff2a457226d7
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-11 17:49:20 +0200

    Change - jquery is included with WP, so remove it and add dependency to parallax

--- peterchrjoergensen/functions.php
+++ peterchrjoergensen/functions.php
@@ -10,11 +10,8 @@ add_action('wp_enqueue_scripts', 'pcj_add_stylesheets');
 
 // Scripts
 function pcj_add_scripts() {
-	wp_register_script('parallax.js', get_template_directory_uri() . '/assets/js/parallax.min.js', array(),'1.0');
+	wp_register_script('parallax.js', get_template_directory_uri() . '/assets/js/parallax.min.js', array('jquery-core'),'1.0');
 	wp_enqueue_script('parallax.js');
-
-	wp_register_script('jquery.js', get_template_directory_uri() . '/assets/js/jquery-1.12.4.min.js', array(),'1.0');
-	wp_enqueue_script('jquery.js');
 
 	wp_register_script('SmoothScroll.js', get_template_directory_uri() . '/assets/js/SmoothScroll.min.js', array(),'1.0');
 	wp_enqueue_script('SmoothScroll.js');
