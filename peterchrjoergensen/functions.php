e79c7bcb62c3adaf247b3f0658af022a13078e2d
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-11 17:40:35 +0200

    Add - smooth scroll

--- peterchrjoergensen/functions.php
+++ peterchrjoergensen/functions.php
@@ -15,6 +15,9 @@ function pcj_add_scripts() {
 
 	wp_register_script('jquery.js', get_template_directory_uri() . 'jquery-1.12.4.min.js', array(),'1.0');
 	wp_enqueue_script('jquery.js');
+
+	wp_register_script('SmoothScroll.js', get_template_directory_uri() . 'SmoothScroll.min.js', array(),'1.0');
+	wp_enqueue_script('SmoothScroll.js');
 }
 
 add_action('wp_enqueue_scripts', 'pcj_add_scripts');
