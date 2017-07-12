a7e770f54140aafeab1048420e3da38001fe57d9
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-12 17:06:54 +0200

    Add - paginate links

--- index.php
+++ index.php
@@ -41,6 +41,19 @@
 						<?php
 							endwhile; endif
 						?>
+						<section class="u-text-center">
+							<?php
+
+								global $wp_query;
+
+								echo paginate_links( array(
+									'base' => str_replace( $big = 999999999, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+									'format' => '?paged=%#%',
+									'current' => max( 1, get_query_var('paged') ),
+									'total' => $wp_query->max_num_pages
+								) );
+							?>
+						</section>
 					</section>
 					<?php get_sidebar(); ?>
 				</div>
