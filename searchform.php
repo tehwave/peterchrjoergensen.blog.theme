dd33d60b9e07ba3a9bc6b513595a22c0bf27d8f6
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-14 04:13:03 +0200

    Change - update to use bootstrap

--- searchform.php
+++ searchform.php
@@ -3,12 +3,27 @@
  * Template for displaying search forms in Twenty Seventeen
  */
 ?>
-<form role="search" method="get" class="search-form u-cf" action="<?php echo home_url( '/' ); ?>">
-	<label for="search"><?php echo _x( '', 'label' ) ?></label>
-	<input type="search" class="u-full-width search-field"
-            placeholder="<?php echo esc_attr_x( 'Find posts by title, date, categories...', 'placeholder' ) ?>"
-            value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">
-    <input type="submit" class="search-submit u-full-width button-primary u-pull-right"
-        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>"/>
+<form role="search" method="GET" action="<?php echo home_url('/'); ?>">
+    <div class="input-group">
+        <input
+            type="search"
+            class="form-control"
+            placeholder="<?php echo esc_attr_x('Find posts by title, date, categories...', 'placeholder') ?>"
+            aria-label="<?php echo esc_attr_x('Find posts by title, date, categories...', 'placeholder') ?>"
+            value="<?php echo get_search_query() ?>"
+            name="s"
+            title="<?php echo esc_attr_x('Search for:', 'label') ?>"
+        />
+        <span class="input-group-btn">
+            <input
+                type="submit"
+                class="btn btn-primary"
+                value="<?php echo esc_attr_x('Search', 'submit button') ?>"
+            />
+        </span>
+    </div>
 </form>
+
+
+
+
