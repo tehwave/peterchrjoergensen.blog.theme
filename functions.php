d3a2db31f05063788ccb959fe69348562c750384
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-14 03:20:13 +0200

    Change - fix styling, fix scripts

--- functions.php
+++ functions.php
@@ -1,39 +1,37 @@
 <?php
 
 // Stylesheets
-function pcj_add_stylesheets() {
-	wp_register_style('pcj.css', get_template_directory_uri() . '/resources/css/pcj.min.css');
-	wp_enqueue_style('pcj.css');
+function pcj_add_stylesheets()
+{
+    wp_register_style('pcj.css', get_template_directory_uri() . '/resources/css/pcj.min.css');
+    wp_enqueue_style('pcj.css');
 }
 
 add_action('wp_enqueue_scripts', 'pcj_add_stylesheets');
 
 // Scripts
-function pcj_add_scripts() {
-	wp_register_script('parallax.js', get_template_directory_uri() . '/assets/js/parallax.min.js', array('jquery-core'));
-	wp_enqueue_script('parallax.js');
+function pcj_add_scripts()
+{
+    wp_register_script('parallax.js', get_template_directory_uri() . '/resources/js/parallax.min.js', ['jquery-core']);
+    wp_enqueue_script('parallax.js');
 
-	wp_register_script('SmoothScroll.js', get_template_directory_uri() . '/assets/js/SmoothScroll.min.js');
-	wp_enqueue_script('SmoothScroll.js');
-
-	wp_register_script('SmoothScrolltop.js', get_template_directory_uri() . '/assets/js/SmoothScrolltop.jquery.min.js', array('jquery-core'));
-	wp_enqueue_script('SmoothScrolltop.js');
+    wp_register_script('smoothscroll.js', get_template_directory_uri() . '/resources/js/smoothscroll.min.js');
+    wp_enqueue_script('smoothscroll.js');
 }
 
 add_action('wp_enqueue_scripts', 'pcj_add_scripts');
 
 // Widgets
-if ( function_exists('register_sidebar') ) {
-	register_sidebar( array(
-		'id'            => 'sidebar',
-		'name'          => 'Sidebar',
-		'before_widget' => '',
-		'after_widget'  => '',
-		'before_title'  => '',
-		'after_title'   => '',
-	) );
+if (function_exists('register_sidebar')) {
+    register_sidebar([
+        'id'            => 'sidebar',
+        'name'          => 'Sidebar',
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => '',
+    ]);
 }
 
 // Support
-add_theme_support( 'post-thumbnails' );
-?>
+add_theme_support('post-thumbnails');
