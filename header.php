e9ac70fff5fd564c1ccb7db8b5bd8a4a1d6749c4
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-22 14:55:36 +0200

    Remove - unnecessary filler

--- header.php
+++ header.php
@@ -23,15 +23,7 @@
                             Peter C. Jørgensen | peterchrjoergensen.dk | @tehwave
          -->
 
-        <meta name="title" content="<?php wp_title(); ?>")>
-        <meta name="description" content="<?php bloginfo('description'); ?>">
-        <meta name="author" content="Peter C. Jørgensen">
-
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
         <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Raleway:700,600,400,300'>
-
         <?php wp_head(); ?>
 
         <style type="text/css">
