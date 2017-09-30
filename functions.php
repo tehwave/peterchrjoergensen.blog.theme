0989287802b72d24f52ef2b70e6dea00b4dc9d21
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-09-30 19:54:56 +0200

    Add - fix for captions applying width via style

--- functions.php
+++ functions.php
@@ -39,3 +39,31 @@ if (function_exists('register_sidebar')) {
 // Support
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');;
+
+// Fix Captions w/ Width applied via Style
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+    // New-style shortcode with the caption inside the shortcode with the link and image tags.
+    if ( ! isset( $attr['caption'] ) ) {
+        if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+            $content = $matches[1];
+            $attr['caption'] = trim( $matches[2] );
+        }
+    }
+    // Allow plugins/themes to override the default caption template.
+    $output = apply_filters('img_caption_shortcode', '', $attr, $content);
+    if ( $output != '' )
+        return $output;
+    extract(shortcode_atts(array(
+        'id'      => '',
+        'align'   => 'alignnone',
+        'width'   => '',
+        'caption' => ''
+    ), $attr));
+    if ( 1 > (int) $width || empty($caption) )
+        return $content;
+    if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+    return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: auto">'
+    . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
